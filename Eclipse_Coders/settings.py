bef744e74446e6c0b073bb0097d7a1cd915fdd82
elithecompguru <eliudmaina064@gmail.com>
2021-04-05 16:40:14 +0300

    Initial commit

--- Eclipse_Coders/settings.py
+++ Eclipse_Coders/settings.py
@@ -12,11 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
@@ -33,9 +32,9 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'qq=aeg5=!6+puu+#^xp%nup1cyn0sf9@b3dugkus^anq9!(6wf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -148,4 +147,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = 'default from email'
+
+django_heroku.settings(locals())
 
